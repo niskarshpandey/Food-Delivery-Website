ccb218858ed3d5b7614cb15c655c1a28c66a766c
Niskarsh Mani Pandey <niskarshpandey0@gmail.com>
2025-09-03 00:00:32 +0530

    Updated project: deleted old components, added new Context folder, and modified Home page

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import image1 from "../assets/image1.avif"
+import { LuLeafyGreen } from "react-icons/lu";
+import { GiChickenOven } from "react-icons/gi";
 
-const Card = () => {
+function Card ({name,image,id,price,type}) {
   return (
-    <div>
-      
+    <div className='w-[250px] h-[350px] bg-white p-3 rounded-md flex flex-col gap-3 shadow-lg hover:border-2 border-orange-300 '>
+        <div className='w-[100%] h-[60%] overflow-hidden'>
+            <img src={image} alt="" className='object-cover rounded-md'/>
+        </div>
+        <div className='text-xl font-semibold'>
+            {name}
+        </div>
+        <div className='w-full flex justify-between items-center'>
+            <div className='text-lg font-bold text-orange-500'>Rs {price}/-</div>
+            <div className='flex justify-center items-center gap-2 text-orange-500 text-lg font-semibold'>
+                {type=="veg"?<LuLeafyGreen/>:<GiChickenOven/>}
+                <span>{type}</span>
+            </div>
+        </div>
+        <button className='w-full p-3 rounded-md bg-orange-500 text-white hover:bg-orange-400 
+        transition-all cursor-pointer duration-200'>Add to dish</button>
     </div>
   )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,28 +1,38 @@
-import React from 'react'
-import Nav from '../Components/nav'
-import Categories from '../Category'
+import React, { useState } from 'react'
+import Nav from '../Components/Nav'
+import Categories from '../Categories'
+import Card from '../Components/Card'
+import { food_items } from '../food'
 
-const Home = () => {
+function Home ()  {
+    let [cate, setCate] = useState(food_items)
+
+    function filter(category){
+        if(category==="All"){
+            setCate(food_items)
+        }else{
+           let newList = food_items.filter((item)=>(item.food_category===category ))
+           setCate(newList)
+        }
+    }
   return (
-    <div className='flex flex-wrap justify-center items-center gap-5 w-[100%] ml-6'>
-      {/* Navigation Bar */}
-      <Nav/>
-
-      {/* Categories Horizontal Scroll */}
-      <div className='flex overflow-x-auto gap-4 p-4 pl-6 scrollbar-hide'>
-        {Categories.map((item) => {
-          return (
-               <div 
-      className="w-[140px] h-[130px] bg-white flex flex-col justify-center items-center 
-                 rounded-lg shadow-md hover:bg-orange-100 hover:scale-105 transition-all duration-300"
-    >
-      {item.image}
-      <span className="mt-2 font-semibold">{item.name}</span>
-    </div>
-
-          )
-        })}
-      </div>
+    <div className='w-full min-h-[100vh] bg-slate-200'>
+        <Nav/>
+        <div className='flex flex-wrap justify-center items-center gap-5 w-[100%] '>
+            {Categories.map((item)=>{
+                return <div className='w-[140px] h-[130px] bg-white flex flex-col items-start gap-5 p-5 justify-start 
+                text-[17px] text-gray-600 rounded-md shadow-xl hover:bg-orange-200 
+                cursor-pointer transition-all duration-200' onClick={()=>filter(item.name)}>
+                    {item.image}
+                    {item.name}
+                </div>
+            })}
+        </div>
+        <div className='w-full flex flex-wrap gap-5 px-5 justify-center items-center pt-8 pb-8'>
+            {cate.map((item)=>(
+                <Card name={item.food_name} image={item.food_image} price={item.price} id={item.id} type={item.food_type}/>
+            ))}
+        </div>
     </div>
   )
 }
